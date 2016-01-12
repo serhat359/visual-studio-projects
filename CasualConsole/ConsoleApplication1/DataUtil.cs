9b2d8b3fc8d47b4421ca11d35d3f7d452dc71341
Serhat Abdulbakioğlu <serhat359@gmail.com>
2016-01-12 23:05:59 +0200

    yeni pivot metodu yazıldı

--- CasualConsole/ConsoleApplication1/DataUtil.cs
+++ CasualConsole/ConsoleApplication1/DataUtil.cs
@@ -10,9 +10,7 @@ namespace ConsoleApplication1
 {
     class DataUtil
     {
-        private static Dictionary<string, List<object>> keyToValues = new Dictionary<string, List<object>>();
-
-        public static DataTable Pivot<T, C, V>(ICollection<T> elements, Expression<Func<T, C>> columnNameProperty, Expression<Func<T, V>> valueProperty, Func<IEnumerable<V>, string> reduceMethod, string defaultvalue, Func<C, string> valueFormat = null)
+        public static DataTable PivotAll<T, C, V>(ICollection<T> elements, Expression<Func<T, C>> columnNameProperty, Expression<Func<T, V>> valueProperty, Func<IEnumerable<V>, string> reduceMethod, string defaultvalue, Func<C, string> valueFormat = null)
         {
             List<PropertyInfo> otherProperties = GetOtherProperties<T, C, V>(columnNameProperty, valueProperty);
 
@@ -29,6 +27,48 @@ namespace ConsoleApplication1
 
             return table;
         }
+
+        public static DataTable Pivot<T, RH, CH, V, O>(IEnumerable<T> sourceElements, Expression<Func<T, RH>> rowProperty, Func<T, CH> columnProperty, Func<T, V> valueProperty, Func<IEnumerable<V>, O> valueReducer, O defaultValue, Func<CH, string> columnNameFormatter) where CH : IComparable<CH>
+        {
+            Func<T, RH> rowSelector = rowProperty.Compile();
+            List<CH> distinctColumnNames = sourceElements.Select(columnProperty).Distinct().ToList();
+
+            PropertyInfo columnMember = (rowProperty.Body as MemberExpression).Member as PropertyInfo;
+
+            DataTable table = new DataTable();
+            table.Columns.Add(columnMember.Name, columnMember.PropertyType);
+            foreach (CH item in distinctColumnNames)
+            {
+                table.Columns.Add(columnNameFormatter(item), typeof(O));
+            }
+
+            var groupedBy = sourceElements.GroupBy(rowSelector).ToList();
+
+            foreach (var groupPair in groupedBy)
+            {
+                RH rowHeader = groupPair.Key;
+                List<T> groupedSourceElements = groupPair.ToList();
+
+                object[] rowValues = new object[distinctColumnNames.Count + 1];
+                rowValues[0] = rowHeader;
+
+                int i = 1;
+                foreach (CH columnHeader in distinctColumnNames)
+                {
+                    IEnumerable<V> values = groupedSourceElements.Where(x => columnProperty(x).CompareTo(columnHeader) == 0).Select(valueProperty);
+                    O outputValue = values.Any() ? valueReducer(values) : defaultValue;
+                    rowValues[i] = outputValue;
+                    i++;
+                }
+
+                table.Rows.Add(rowValues);
+            }
+
+            return table;
+        }
+
+        #region Private Methods
+        private static Dictionary<string, List<object>> keyToValues = new Dictionary<string, List<object>>();
 
         private static void AddValuesToTable<C, V>(Func<IEnumerable<V>, object> reduceMethod, string defaultvalue, List<C> distinctColumnValues, DataTable table, Dictionary<string, Dictionary<C, List<V>>> valueDic)
         {
@@ -115,6 +155,6 @@ namespace ConsoleApplication1
             List<PropertyInfo> otherProperties = allProperties.Where(x => x.Name != columnNamePropertyInfo.Name && x.Name != valuePropertyInfo.Name).ToList();
             return otherProperties;
         }
-
+        #endregion
     }
 }
