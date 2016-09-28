163c7ab90fb963cbdbdc6b965c5d170399bec2b2
Serhat Abdulbakioğlu <serhat359@gmail.com>
2016-09-28 23:31:04 +0300

    takvim eklendi

--- Nobetci/Nobetci/Form1.Designer.cs
+++ Nobetci/Nobetci/Form1.Designer.cs
@@ -29,7 +29,16 @@
         private void InitializeComponent()
         {
             this.nobetTable = new System.Windows.Forms.DataGridView();
+            this.myCalendar = new System.Windows.Forms.DataGridView();
+            this.Pzt = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Sal = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Çar = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Per = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Cum = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Cts = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Paz = new System.Windows.Forms.DataGridViewTextBoxColumn();
             ((System.ComponentModel.ISupportInitialize)(this.nobetTable)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.myCalendar)).BeginInit();
             this.SuspendLayout();
             // 
             // nobetTable
@@ -43,15 +52,83 @@
             this.nobetTable.Size = new System.Drawing.Size(482, 405);
             this.nobetTable.TabIndex = 0;
             // 
+            // myCalendar
+            // 
+            this.myCalendar.AllowUserToAddRows = false;
+            this.myCalendar.AllowUserToDeleteRows = false;
+            this.myCalendar.AllowUserToResizeColumns = false;
+            this.myCalendar.AllowUserToResizeRows = false;
+            this.myCalendar.AutoSizeColumnsMode = System.Windows.Forms.DataGridViewAutoSizeColumnsMode.Fill;
+            this.myCalendar.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.myCalendar.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
+            this.Pzt,
+            this.Sal,
+            this.Çar,
+            this.Per,
+            this.Cum,
+            this.Cts,
+            this.Paz});
+            this.myCalendar.EditMode = System.Windows.Forms.DataGridViewEditMode.EditOnF2;
+            this.myCalendar.Location = new System.Drawing.Point(518, 12);
+            this.myCalendar.Name = "myCalendar";
+            this.myCalendar.ReadOnly = true;
+            this.myCalendar.Size = new System.Drawing.Size(240, 157);
+            this.myCalendar.TabIndex = 2;
+            this.myCalendar.CellContentDoubleClick += new System.Windows.Forms.DataGridViewCellEventHandler(this.myCalendar_CellContentClick);
+            // 
+            // Pzt
+            // 
+            this.Pzt.HeaderText = "Pzt";
+            this.Pzt.Name = "Pzt";
+            this.Pzt.ReadOnly = true;
+            // 
+            // Sal
+            // 
+            this.Sal.HeaderText = "Sal";
+            this.Sal.Name = "Sal";
+            this.Sal.ReadOnly = true;
+            // 
+            // Çar
+            // 
+            this.Çar.HeaderText = "Çar";
+            this.Çar.Name = "Çar";
+            this.Çar.ReadOnly = true;
+            // 
+            // Per
+            // 
+            this.Per.HeaderText = "Per";
+            this.Per.Name = "Per";
+            this.Per.ReadOnly = true;
+            // 
+            // Cum
+            // 
+            this.Cum.HeaderText = "Cum";
+            this.Cum.Name = "Cum";
+            this.Cum.ReadOnly = true;
+            // 
+            // Cts
+            // 
+            this.Cts.HeaderText = "Cts";
+            this.Cts.Name = "Cts";
+            this.Cts.ReadOnly = true;
+            // 
+            // Paz
+            // 
+            this.Paz.HeaderText = "Paz";
+            this.Paz.Name = "Paz";
+            this.Paz.ReadOnly = true;
+            // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(834, 429);
+            this.Controls.Add(this.myCalendar);
             this.Controls.Add(this.nobetTable);
             this.Name = "Form1";
             this.Text = "Form1";
             ((System.ComponentModel.ISupportInitialize)(this.nobetTable)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.myCalendar)).EndInit();
             this.ResumeLayout(false);
 
         }
@@ -59,6 +136,14 @@
         #endregion
 
         private System.Windows.Forms.DataGridView nobetTable;
+        private System.Windows.Forms.DataGridView myCalendar;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Pzt;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Sal;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Çar;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Per;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Cum;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Cts;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Paz;
     }
 }
 
--- Nobetci/Nobetci/Form1.cs
+++ Nobetci/Nobetci/Form1.cs
@@ -15,14 +15,48 @@ namespace Nobetci
         {
             InitializeComponent();
 
-            //this.nobetTable.AllowUserToAddRows = true;
-            //this.nobetTable.AllowUserToDeleteRows = true;
-            //this.nobetTable.AutoGenerateColumns = true;
+            SetupNobetTable();
 
+            SetupCalendar();
+
+            
+        }
+
+        private void SetupCalendar()
+        {
+            const int year = 2016;
+            const int month = 10;
+            DateTime date = new DateTime(year, month, 1);
+            
+            // myCalendar
+            myCalendar.Rows.Add(6);
+
+            int firstDay = (int)date.DayOfWeek;
+            firstDay = firstDay == 0 ? 7 : firstDay;
+
+            for (int i = 0; i < DateTime.DaysInMonth(year, month); i++)
+            {
+                int thisDay = i + firstDay - 1;
+                int row = thisDay / 7;
+                int column = thisDay % 7;
+
+                myCalendar.Rows[row].Cells[column].Value = i + 1;
+
+                var dayOfWeek = new DateTime(year, month, i + 1).DayOfWeek;
+                if (dayOfWeek == DayOfWeek.Saturday || dayOfWeek == DayOfWeek.Sunday)
+                    ToggleHoliday(row, column);
+            }
+        }
+
+        private void SetupNobetTable()
+        {
             DataTable table = new DataTable();
             table.Columns.Add("İsim", typeof(string));
             table.Columns.Add("1.", typeof(int));
             table.Columns.Add("2.", typeof(int));
+            table.Columns.Add("3.", typeof(int));
+            table.Columns.Add("4.", typeof(int));
+            table.Columns.Add("5.", typeof(int));
 
             table.Rows.Add("serhat", 15, 23);
             table.Rows.Add("ahmet", 28, 12);
@@ -30,9 +64,30 @@ namespace Nobetci
 
             this.nobetTable.DataSource = table;
             this.nobetTable.EndEdit();
-            
-            
+        }
 
+        private void myCalendar_CellContentClick(object sender, DataGridViewCellEventArgs e)
+        {
+            ToggleHoliday(e.RowIndex, e.ColumnIndex);
+        }
+
+        private void ToggleHoliday(int row, int column)
+        {
+            var color = myCalendar.Rows[row].Cells[column].Style.ForeColor;
+
+            if (color != Color.Red)
+            {
+                myCalendar.Rows[row].Cells[column].Style.ForeColor = Color.Red;
+            }
+            else
+            {
+                myCalendar.Rows[row].Cells[column].Style.ForeColor = Color.Black;
+            }
+        }
+
+        private bool IsHoliday(int row, int column)
+        {
+            return myCalendar.Rows[row].Cells[column].Style.ForeColor == Color.Red;
         }
     }
 }
