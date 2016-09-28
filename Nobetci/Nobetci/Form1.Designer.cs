86cccba2e4c63860af383eb00cba65150e7fcb8a
Serhat Abdulbakioğlu <serhat359@gmail.com>
2016-09-28 22:37:24 +0300

    dosyalar indexe eklendi

---
+++ Nobetci/Nobetci/Form1.Designer.cs
@@ -0,0 +1,64 @@
+﻿namespace Nobetci
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.nobetTable = new System.Windows.Forms.DataGridView();
+            ((System.ComponentModel.ISupportInitialize)(this.nobetTable)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // nobetTable
+            // 
+            this.nobetTable.AllowUserToResizeRows = false;
+            this.nobetTable.AutoSizeColumnsMode = System.Windows.Forms.DataGridViewAutoSizeColumnsMode.Fill;
+            this.nobetTable.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
+            this.nobetTable.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.nobetTable.Location = new System.Drawing.Point(12, 12);
+            this.nobetTable.Name = "nobetTable";
+            this.nobetTable.Size = new System.Drawing.Size(482, 405);
+            this.nobetTable.TabIndex = 0;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(834, 429);
+            this.Controls.Add(this.nobetTable);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.nobetTable)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.DataGridView nobetTable;
+    }
+}
+
---
+++ Nobetci/Nobetci/Form1.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+namespace Nobetci
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+
+            //this.nobetTable.AllowUserToAddRows = true;
+            //this.nobetTable.AllowUserToDeleteRows = true;
+            //this.nobetTable.AutoGenerateColumns = true;
+
+            DataTable table = new DataTable();
+            table.Columns.Add("İsim", typeof(string));
+            table.Columns.Add("1.", typeof(int));
+            table.Columns.Add("2.", typeof(int));
+
+            table.Rows.Add("serhat", 15, 23);
+            table.Rows.Add("ahmet", 28, 12);
+            table.AcceptChanges();
+
+            this.nobetTable.DataSource = table;
+            this.nobetTable.EndEdit();
+            
+            
+
+        }
+    }
+}
