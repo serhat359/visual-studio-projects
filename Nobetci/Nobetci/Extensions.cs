e1956c00e038d5e98480e48f4deb03003c1b517f
Serhat Abdulbakioğlu <serhat359@gmail.com>
2016-10-02 15:37:59 +0300

    ay ve yıl alma eksik commit

---
+++ Nobetci/Nobetci/Extensions.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Nobetci
+{
+    public static class Extensions
+    {
+        public class IndexValuePair<T>
+        {
+            public int Index { get; set; }
+            public T Value { get; set; }
+        }
+
+        public static IEnumerable<IndexValuePair<T>> WithIndex<T>(this IEnumerable<T> elems)
+        {
+            int i = 0;
+            foreach (var elem in elems)
+            {
+                yield return new IndexValuePair<T> { Index = i++, Value = elem };
+            }
+        }
+
+        public static int? CastInt(object o) {
+            try
+            {
+                return (int)o;
+            }
+            catch (Exception)
+            {
+                return null;
+            }
+        }
+    }
+}
---
+++ Nobetci/Nobetci/YearAndMonthForm.Designer.cs
@@ -0,0 +1,83 @@
+﻿namespace Nobetci
+{
+    partial class YearAndMonthForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.comboBoxYear = new System.Windows.Forms.ComboBox();
+            this.comboBoxMonth = new System.Windows.Forms.ComboBox();
+            this.okButton = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // comboBoxYear
+            // 
+            this.comboBoxYear.FormattingEnabled = true;
+            this.comboBoxYear.Location = new System.Drawing.Point(12, 12);
+            this.comboBoxYear.Name = "comboBoxYear";
+            this.comboBoxYear.Size = new System.Drawing.Size(121, 21);
+            this.comboBoxYear.TabIndex = 0;
+            this.comboBoxYear.Text = "Yıl";
+            // 
+            // comboBoxMonth
+            // 
+            this.comboBoxMonth.FormattingEnabled = true;
+            this.comboBoxMonth.Location = new System.Drawing.Point(140, 12);
+            this.comboBoxMonth.Name = "comboBoxMonth";
+            this.comboBoxMonth.Size = new System.Drawing.Size(121, 21);
+            this.comboBoxMonth.TabIndex = 1;
+            this.comboBoxMonth.Text = "Ay";
+            // 
+            // okButton
+            // 
+            this.okButton.Location = new System.Drawing.Point(268, 13);
+            this.okButton.Name = "okButton";
+            this.okButton.Size = new System.Drawing.Size(75, 23);
+            this.okButton.TabIndex = 2;
+            this.okButton.Text = "Tamam";
+            this.okButton.UseVisualStyleBackColor = true;
+            // 
+            // YearAndMonthForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(377, 70);
+            this.Controls.Add(this.okButton);
+            this.Controls.Add(this.comboBoxMonth);
+            this.Controls.Add(this.comboBoxYear);
+            this.Name = "YearAndMonthForm";
+            this.Text = "YearAndMonthForm";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ComboBox comboBoxYear;
+        private System.Windows.Forms.ComboBox comboBoxMonth;
+        private System.Windows.Forms.Button okButton;
+    }
+}
