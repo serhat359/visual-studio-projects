1b03f37ee2ae351baec7b21af76ff7e5f7ad7b6b
Serhat Abdulbakioğlu <serhat359@gmail.com>
2017-01-15 22:30:41 +0300

    xml beautifier initial code

---
+++ CasualConsole/XMLBeautifier/Form1.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+using System.IO;
+using System.Xml;
+
+namespace XMLBeautifier
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void richTextBox1_TextChanged(object sender, EventArgs e)
+        {
+            string newStr = PrintXML(((RichTextBox)sender).Text);
+
+            richTextBox2.Text = newStr;
+        }
+
+        public static String PrintXML(String XML)
+        {
+            String result = "";
+
+            MemoryStream mStream = new MemoryStream();
+            XmlTextWriter writer = new XmlTextWriter(mStream, Encoding.Unicode);
+            XmlDocument document = new XmlDocument();
+
+            try
+            {
+                // Load the XmlDocument with the XML.
+                document.LoadXml(XML);
+
+                writer.Formatting = Formatting.Indented;
+
+                // Write the XML into a formatting XmlTextWriter
+                document.WriteContentTo(writer);
+                writer.Flush();
+                mStream.Flush();
+
+                // Have to rewind the MemoryStream in order to read
+                // its contents.
+                mStream.Position = 0;
+
+                // Read MemoryStream contents into a StreamReader.
+                StreamReader sReader = new StreamReader(mStream);
+
+                // Extract the text from the StreamReader.
+                String formattedXML = sReader.ReadToEnd();
+
+                result = formattedXML.Replace("  ","\t");
+
+                mStream.Close();
+                writer.Close();
+            }
+            catch (XmlException)
+            {
+            }
+
+            return result;
+        }
+    }
+}
