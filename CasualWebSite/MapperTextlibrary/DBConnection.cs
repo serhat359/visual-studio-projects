014650dc94f2207a17ba44fea43a82f46f454185
Serhat Abdulbakioğlu <serhat359@gmail.com>
2017-01-15 22:35:22 +0300

    bir satır update eden query yazıldı

--- CasualWebSite/MapperTextlibrary/DBConnection.cs
+++ CasualWebSite/MapperTextlibrary/DBConnection.cs
@@ -9,89 +9,111 @@ using System.Configuration;
 
 namespace MapperTextlibrary
 {
-	public class DBConnection
-	{
-		public enum Mode
-		{
-			Read,
-			Write
-		}
+    public class DBConnection
+    {
+        public enum Mode
+        {
+            Read,
+            Write
+        }
 
-		private MySqlConnection connection;
+        private MySqlConnection connection;
 
-		private const int CONNECTION_ERROR = 0;
-		private const int INVALID_USERNAME_PASSWORD = 1045;
+        private const int CONNECTION_ERROR = 0;
+        private const int INVALID_USERNAME_PASSWORD = 1045;
 
-		public DBConnection(Mode mode)
-		{
-			Initialize(mode);
-		}
+        public DBConnection(Mode mode)
+        {
+            Initialize(mode);
+        }
 
-		public LinkedList<T> RunSelectQuery<T>(string sql, IMapper<T> mapper) where T : new()
-		{
-			OpenConnection();
+        public LinkedList<T> RunSelectQuery<T>(string sql, IMapper<T> mapper) where T : new()
+        {
+            OpenConnection();
 
-			DbCommand cmd = new MySqlCommand(sql, connection);
+            DbCommand cmd = new MySqlCommand(sql, connection);
 
-			IDataReader dataReader = cmd.ExecuteReader();
+            IDataReader dataReader = cmd.ExecuteReader();
 
-			LinkedList<T> list = mapper.MapAll(dataReader);
+            LinkedList<T> list = mapper.MapAll(dataReader);
 
-			dataReader.Close();
+            dataReader.Close();
 
-			this.CloseConnection();
+            this.CloseConnection();
 
-			return list;
-		}
+            return list;
+        }
 
-		private void Initialize(Mode mode)
-		{
-			ConnectionStringSettings connectionStringSetting = GetConnectionString(mode);
+        private void Initialize(Mode mode)
+        {
+            ConnectionStringSettings connectionStringSetting = GetConnectionString(mode);
 
-			connection = new MySqlConnection(connectionStringSetting.ConnectionString);
-		}
+            connection = new MySqlConnection(connectionStringSetting.ConnectionString);
+        }
 
-		private ConnectionStringSettings GetConnectionString(Mode mode)
-		{
-			switch (mode)
-			{
-				case Mode.Read:
-					return ConfigurationManager.ConnectionStrings["MySQLReaderConnection"];
-				case Mode.Write:
-					return ConfigurationManager.ConnectionStrings["MySQLWriterConnection"];
-				default:
-					throw new Exception("Invalid operation mode");
-			}
-		}
+        private ConnectionStringSettings GetConnectionString(Mode mode)
+        {
+            switch (mode)
+            {
+                case Mode.Read:
+                    return ConfigurationManager.ConnectionStrings["MySQLReaderConnection"];
+                case Mode.Write:
+                    return ConfigurationManager.ConnectionStrings["MySQLWriterConnection"];
+                default:
+                    throw new Exception("Invalid operation mode");
+            }
+        }
 
-		private void OpenConnection()
-		{
-			try
-			{
-				connection.Open();
-			}
-			catch (MySqlException ex)
-			{
-				switch (ex.Number)
-				{
-					case CONNECTION_ERROR:
-						throw new Exception("Cannot connect to server.  Contact administrator", ex);
-					case INVALID_USERNAME_PASSWORD:
-						throw new Exception("Invalid username/password, please try again", ex);
-				}
-			}
-		}
+        private void OpenConnection()
+        {
+            try
+            {
+                connection.Open();
+            }
+            catch (MySqlException ex)
+            {
+                switch (ex.Number)
+                {
+                    case CONNECTION_ERROR:
+                        throw new Exception("Cannot connect to server.  Contact administrator", ex);
+                    case INVALID_USERNAME_PASSWORD:
+                        throw new Exception("Invalid username/password, please try again", ex);
+                }
+            }
+        }
 
-		private void CloseConnection()
-		{
-			try
-			{
-				connection.Close();
-			}
-			catch (MySqlException ex)
-			{
-				throw new Exception("Error when closing the connection", ex);
-			}
-		}
-	}
+        private void CloseConnection()
+        {
+            try
+            {
+                connection.Close();
+            }
+            catch (MySqlException ex)
+            {
+                throw new Exception("Error when closing the connection", ex);
+            }
+        }
+
+        public void RunUpsertQueryOneRow(string sql)
+        {
+            OpenConnection();
+
+            var trans = connection.BeginTransaction();
+
+            DbCommand cmd = new MySqlCommand(sql, connection);
+
+            int numberOfRecords = cmd.ExecuteNonQuery();
+
+            if (numberOfRecords == 1)
+            {
+                trans.Commit();
+                return;
+            }
+            else
+            {
+                trans.Rollback();
+                throw new Exception(string.Format("Affected number of rows was not expected: {0}", numberOfRecords));
+            }
+        }
+    }
 }
