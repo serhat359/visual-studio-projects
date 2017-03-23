f628b504c3144d01b06a6eefd0887110328e7ffa
Serhat Abdulbakioğlu <serhat359@gmail.com>
2017-03-23 10:40:49 +0300

    GenerateCRC32Checksum projesi eklendi

---
+++ CasualConsole/GenerateCRC32Checksum/Program.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.IO;
+
+namespace GenerateCRC32Checksum
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string path = Directory.GetCurrentDirectory() + "\\";
+
+            string[] filenames = Directory.GetFiles(path, "*", SearchOption.AllDirectories);
+
+            var codelist = filenames.Select(x => new { filename = x.Replace(path,""), code = GetCRC(x) }).Where(x => x.code != null).ToList();
+
+            if (codelist.Count > 0)
+            {
+                string sfvFileName = "Checksum.sfv";
+                string sfvPath = Path.Combine(path, sfvFileName);
+
+                File.WriteAllLines(sfvPath, codelist.Select(x => string.Format("{0} {1}", x.filename, x.code)), Encoding.UTF8);
+            }
+        }
+
+        static string GetCRC(string filename)
+        {
+            int length = filename.Length;
+
+            for (int i = 0; i < length; i++)
+            {
+                int bracIndex = filename.IndexOf('[', i);
+
+                if (bracIndex >= 0)
+                {
+                    int bracEndIndex = filename.IndexOf(']', bracIndex);
+
+                    if (bracEndIndex - bracIndex == 9)
+                    {
+                        string code = filename.Substring(bracIndex + 1, 8);
+                        if (code.All(c => IsHexadecimal(c)))
+                            return code;
+                    }
+                }
+                else
+                    break;
+
+                i = bracIndex + 1;
+            }
+
+            return null;
+        }
+
+        private static bool IsHexadecimal(char c)
+        {
+            Func<char, char, char, bool> between = (ch, start, end) => ch >= start && ch <= end;
+
+            return between(c, '0', '9') || between(c, 'a', 'f') || between(c, 'A', 'F');
+        }
+    }
+}
---
+++ CasualConsole/GenerateCRC32Checksum/Properties/AssemblyInfo.cs
@@ -0,0 +1,36 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following 
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("GenerateCRC32Checksum")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("GenerateCRC32Checksum")]
+[assembly: AssemblyCopyright("Copyright ©  2017")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible 
+// to COM components.  If you need to access a type in this assembly from 
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("1ea56ded-1888-4cbf-802e-54e95d30378b")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version 
+//      Build Number
+//      Revision
+//
+// You can specify all the values or you can default the Build and Revision Numbers 
+// by using the '*' as shown below:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
