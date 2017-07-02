730981de87d77db6538d1e8bae8559abde3f98c9
Serhat Abdulbakioğlu <serhat359@gmail.com>
2017-07-02 20:05:02 +0300

    hızlandırma bir bugdan dolayı askıya alındı, daha sonra bakılacak

--- CasualConsole/PicrossSolver/Generic.cs
+++ CasualConsole/PicrossSolver/Generic.cs
@@ -187,13 +187,17 @@ namespace PicrossSolver
         {
             bool executeBelow = true;
 
-            for (int row = 0; executeBelow && row < Form1.rowCount && !Form1.isRowCompleted[row]; row++)
+            for (int row = 0; executeBelow && row < Form1.rowCount 
+                //&& !Form1.isRowCompleted[row]
+                ; row++)
             {
                 processing(new Form1.CellSeries(row, picture, Form1.Direction.Horizontal, leftColumn[row]));
                 processing(new Form1.CellSeries(row, picture, Form1.Direction.HorizontalReverse, leftColumn[row]));
             }
 
-            for (int col = 0; executeBelow && col < Form1.rowCount && !Form1.isColCompleted[col]; col++)
+            for (int col = 0; executeBelow && col < Form1.rowCount 
+                //&& !Form1.isColCompleted[col]
+                ; col++)
             {
                 processing(new Form1.CellSeries(col, picture, Form1.Direction.Vertical, upColumn[col]));
                 processing(new Form1.CellSeries(col, picture, Form1.Direction.VerticalReverse, upColumn[col]));
