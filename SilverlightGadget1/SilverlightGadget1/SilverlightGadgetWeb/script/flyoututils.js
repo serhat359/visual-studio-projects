9d2dcde934a6561dea268ebe0a51d39f9374691a
Serhat Abdulbakioğlu <serhat359@gmail.com>
2017-07-05 17:08:05 +0300

    deneme amaçlı oluşturulan silverlight projesi silindi

--- SilverlightGadget1/SilverlightGadget1/SilverlightGadgetWeb/script/flyoututils.js
+++
@@ -1,7 +0,0 @@
-﻿// Silverlight Vista Sidebar Gadget Template created by Ioan Lazarciuc
-// http://www.lazarciuc.ro/ioan
-// Contact form present on website
-// Based on the Vista Sidebar Gadget Template created by Tim Heuer 
-// http://timheuer.com/blog/
-
-// Put any required JavaScript code for the flyout here
