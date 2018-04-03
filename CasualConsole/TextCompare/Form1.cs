4aa0838bc563dfdc6bbac490bbbf2e4b5bc50727
Serhat Abdulbakioğlu <serhat359@gmail.com>
2018-04-03 22:08:29 +0300

    textcompare projesi eklendi

---
+++ CasualConsole/TextCompare/Form1.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Diagnostics;
+using System.IO;
+using System.Windows.Forms;
+
+namespace TextCompare
+{
+    public partial class TextCompare : Form
+    {
+        public TextCompare()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            var tempPath = System.IO.Path.GetTempPath();
+
+            string txt1Path = Path.Combine(tempPath, "file1.txt");
+            string txt2Path = Path.Combine(tempPath, "file2.txt");
+
+            File.WriteAllText(txt1Path, richTextBox1.Text);
+            File.WriteAllText(txt2Path, richTextBox2.Text);
+
+            Process.Start("tortoisegitmerge", txt1Path + " " + txt2Path);
+        }
+    }
+}
