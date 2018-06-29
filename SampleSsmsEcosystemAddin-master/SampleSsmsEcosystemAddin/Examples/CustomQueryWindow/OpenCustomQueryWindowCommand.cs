9dea4d46723ee3083b642ee702588d4a33d9b6f1
Serhat Abdulbakioğlu <serhat359@gmail.com>
2018-06-29 21:50:51 +0300

    ssms sample proje oluşturuldu

---
+++ SampleSsmsEcosystemAddin-master/SampleSsmsEcosystemAddin/Examples/CustomQueryWindow/OpenCustomQueryWindowCommand.cs
@@ -0,0 +1,48 @@
+using System.Reflection;
+using System.Windows.Forms;
+using RedGate.SIPFrameworkShared;
+
+namespace SampleSsmsEcosystemAddin.Examples.CustomQueryWindow
+{
+    internal class OpenCustomQueryWindowCommand : ISharedCommandWithExecuteParameter, ISharedCommand
+    {
+        private readonly ISsmsFunctionalityProvider6 m_Provider;
+        private readonly ICommandImage m_CommandImage = new CommandImageForEmbeddedResources(Assembly.GetExecutingAssembly(), "SampleSsmsEcosystemAddin.Examples.rg_icon.ico");
+
+        public OpenCustomQueryWindowCommand(ISsmsFunctionalityProvider6 provider)
+        {
+            m_Provider = provider;
+        }
+
+        public string Name { get { return "RedGate_Sample_OpenCustomQueryWindow"; } }
+
+        public void Execute(object parameter)
+        {
+            Execute();
+        }
+
+public void Execute()
+{
+    var control = new CustomQueryWindowControl(m_Provider);
+    m_Provider.GetQueryWindowManager().CreateAugmentedQueryWindow(string.Empty, "Custom query window", control);
+    control.Dock = DockStyle.Bottom;
+    GetConnection(control);
+}
+
+private void GetConnection(CustomQueryWindowControl control)
+{
+    var parent = control.Parent;
+    var type = parent.GetType();
+    var connectionProperty = type.GetProperty("Connection");
+    var connection = connectionProperty.GetValue(parent, new object[] {});
+    //connection has all the information in it
+}
+
+        public string Caption { get { return "Open Custom Query Window"; } }
+        public string Tooltip { get { return "Tooltip"; }}
+        public ICommandImage Icon { get { return m_CommandImage; } }
+        public string[] DefaultBindings { get { return new[] { "global::Ctrl+Alt+J" }; } }
+        public bool Visible { get { return true; } }
+        public bool Enabled { get { return true; } }
+    }
+}
---
+++ SampleSsmsEcosystemAddin-master/SampleSsmsEcosystemAddin/Examples/ObjectExplorerMenus/ObjectExplorerSubmenu.cs
@@ -0,0 +1,25 @@
+﻿using System.Reflection;
+using RedGate.SIPFrameworkShared;
+
+namespace SampleSsmsEcosystemAddin.Examples.ObjectExplorerMenus
+{
+    class ObjectExplorerSubmenu : SubmenuSimpleOeMenuItemBase
+    {
+        private readonly ICommandImage m_CommandImage = new CommandImageForEmbeddedResources(Assembly.GetExecutingAssembly(), "SampleSsmsEcosystemAddin.Examples.rg_icon.ico");
+
+        public ObjectExplorerSubmenu(SimpleOeMenuItemBase[] subMenus)
+            : base(subMenus)
+        {
+        }
+
+        public override string ItemText
+        {
+            get { return "SampleExtension"; }
+        }
+
+        public override bool AppliesTo(ObjectExplorerNodeDescriptorBase oeNode)
+        {
+            return GetApplicableChildren(oeNode).Length > 0;
+        }
+    }
+}
