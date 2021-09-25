b65c330bd80378a6b5b336293ab30a479b7b1057
Serhat Abdulbakioğlu <serhat359@gmail.com>
2021-09-25 10:53:22 +0900

    refactor

---
+++ DotNetCoreWebsite/DotNetCoreWebsite/wwwroot/lib/jquery/dist/jquery-ext.js
@@ -0,0 +1,11 @@
+﻿jQuery.fn.visible = function () {
+    return this.css('visibility', 'visible');
+};
+
+jQuery.fn.invisible = function () {
+    return this.css('visibility', 'hidden');
+};
+
+jQuery.fn.setVisible = function (isVisible) {
+    return this.css('visibility', isVisible ? 'visible' :'hidden');
+};
