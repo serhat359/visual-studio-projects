c948c16ddfdb39edf0aea5b7d327b599c81b4c08
Serhat Abdulbakioglu <serhat359@gmail.com>
2023-05-10 21:52:31 +0900

    added complex subtitle fixing

--- CasualConsole/SubtitleFixer/SubtitleFixer.Designer.cs
+++ CasualConsole/SubtitleFixer/SubtitleFixer.Designer.cs
@@ -3,12 +3,12 @@
     partial class SubtitleFixer
     {
         /// <summary>
-        /// Required designer variable.
+        ///  Required designer variable.
         /// </summary>
         private System.ComponentModel.IContainer components = null;
 
         /// <summary>
-        /// Clean up any resources being used.
+        ///  Clean up any resources being used.
         /// </summary>
         /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
         protected override void Dispose(bool disposing)
@@ -28,34 +28,49 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.fixButton = new System.Windows.Forms.Button();
-            this.folderPathTextBox = new System.Windows.Forms.TextBox();
+            this.simpleFixButton = new System.Windows.Forms.Button();
+            this.simpleFolderPathTextBox = new System.Windows.Forms.TextBox();
             this.msTextbox = new System.Windows.Forms.TextBox();
             this.label1 = new System.Windows.Forms.Label();
             this.label2 = new System.Windows.Forms.Label();
+            this.tabControl1 = new System.Windows.Forms.TabControl();
+            this.simpleFixTab = new System.Windows.Forms.TabPage();
+            this.complexFixTab = new System.Windows.Forms.TabPage();
+            this.complexFixButton = new System.Windows.Forms.Button();
+            this.expected2Textbox = new System.Windows.Forms.TextBox();
+            this.inFile2Textbox = new System.Windows.Forms.TextBox();
+            this.label5 = new System.Windows.Forms.Label();
+            this.expected1Textbox = new System.Windows.Forms.TextBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.inFile1Textbox = new System.Windows.Forms.TextBox();
+            this.complexFolderPathTextBox = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.tabControl1.SuspendLayout();
+            this.simpleFixTab.SuspendLayout();
+            this.complexFixTab.SuspendLayout();
             this.SuspendLayout();
             // 
-            // fixButton
+            // simpleFixButton
             // 
-            this.fixButton.Enabled = false;
-            this.fixButton.Location = new System.Drawing.Point(122, 165);
-            this.fixButton.Name = "fixButton";
-            this.fixButton.Size = new System.Drawing.Size(127, 23);
-            this.fixButton.TabIndex = 3;
-            this.fixButton.Text = "Fix";
-            this.fixButton.UseVisualStyleBackColor = true;
-            this.fixButton.Click += new System.EventHandler(this.fixButton_Click);
+            this.simpleFixButton.Enabled = false;
+            this.simpleFixButton.Location = new System.Drawing.Point(91, 115);
+            this.simpleFixButton.Name = "simpleFixButton";
+            this.simpleFixButton.Size = new System.Drawing.Size(127, 23);
+            this.simpleFixButton.TabIndex = 3;
+            this.simpleFixButton.Text = "Fix";
+            this.simpleFixButton.UseVisualStyleBackColor = true;
+            this.simpleFixButton.Click += new System.EventHandler(this.simpleFixButton_Click);
             // 
-            // folderPathTextBox
+            // simpleFolderPathTextBox
             // 
-            this.folderPathTextBox.Location = new System.Drawing.Point(122, 71);
-            this.folderPathTextBox.Name = "folderPathTextBox";
-            this.folderPathTextBox.Size = new System.Drawing.Size(361, 20);
-            this.folderPathTextBox.TabIndex = 2;
+            this.simpleFolderPathTextBox.Location = new System.Drawing.Point(91, 21);
+            this.simpleFolderPathTextBox.Name = "simpleFolderPathTextBox";
+            this.simpleFolderPathTextBox.Size = new System.Drawing.Size(361, 20);
+            this.simpleFolderPathTextBox.TabIndex = 2;
             // 
             // msTextbox
             // 
-            this.msTextbox.Location = new System.Drawing.Point(122, 119);
+            this.msTextbox.Location = new System.Drawing.Point(91, 69);
             this.msTextbox.Name = "msTextbox";
             this.msTextbox.Size = new System.Drawing.Size(127, 20);
             this.msTextbox.TabIndex = 4;
@@ -64,7 +79,7 @@
             // label1
             // 
             this.label1.AutoSize = true;
-            this.label1.Location = new System.Drawing.Point(52, 74);
+            this.label1.Location = new System.Drawing.Point(21, 24);
             this.label1.Name = "label1";
             this.label1.Size = new System.Drawing.Size(23, 13);
             this.label1.TabIndex = 5;
@@ -73,36 +88,172 @@
             // label2
             // 
             this.label2.AutoSize = true;
-            this.label2.Location = new System.Drawing.Point(52, 122);
+            this.label2.Location = new System.Drawing.Point(21, 72);
             this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(64, 13);
+            this.label2.Size = new System.Drawing.Size(51, 13);
             this.label2.TabIndex = 6;
-            this.label2.Text = "Milliseconds";
+            this.label2.Text = "Millis Add";
+            // 
+            // tabControl1
+            // 
+            this.tabControl1.Controls.Add(this.simpleFixTab);
+            this.tabControl1.Controls.Add(this.complexFixTab);
+            this.tabControl1.Location = new System.Drawing.Point(12, 12);
+            this.tabControl1.Name = "tabControl1";
+            this.tabControl1.SelectedIndex = 0;
+            this.tabControl1.Size = new System.Drawing.Size(776, 426);
+            this.tabControl1.TabIndex = 7;
+            // 
+            // simpleFixTab
+            // 
+            this.simpleFixTab.Controls.Add(this.msTextbox);
+            this.simpleFixTab.Controls.Add(this.label2);
+            this.simpleFixTab.Controls.Add(this.simpleFolderPathTextBox);
+            this.simpleFixTab.Controls.Add(this.label1);
+            this.simpleFixTab.Controls.Add(this.simpleFixButton);
+            this.simpleFixTab.Location = new System.Drawing.Point(4, 22);
+            this.simpleFixTab.Name = "simpleFixTab";
+            this.simpleFixTab.Padding = new System.Windows.Forms.Padding(3);
+            this.simpleFixTab.Size = new System.Drawing.Size(768, 400);
+            this.simpleFixTab.TabIndex = 0;
+            this.simpleFixTab.Text = "Simple Fix";
+            this.simpleFixTab.UseVisualStyleBackColor = true;
+            // 
+            // complexFixTab
+            // 
+            this.complexFixTab.Controls.Add(this.complexFixButton);
+            this.complexFixTab.Controls.Add(this.expected2Textbox);
+            this.complexFixTab.Controls.Add(this.inFile2Textbox);
+            this.complexFixTab.Controls.Add(this.label5);
+            this.complexFixTab.Controls.Add(this.expected1Textbox);
+            this.complexFixTab.Controls.Add(this.label4);
+            this.complexFixTab.Controls.Add(this.inFile1Textbox);
+            this.complexFixTab.Controls.Add(this.complexFolderPathTextBox);
+            this.complexFixTab.Controls.Add(this.label3);
+            this.complexFixTab.Location = new System.Drawing.Point(4, 22);
+            this.complexFixTab.Name = "complexFixTab";
+            this.complexFixTab.Padding = new System.Windows.Forms.Padding(3);
+            this.complexFixTab.Size = new System.Drawing.Size(768, 400);
+            this.complexFixTab.TabIndex = 1;
+            this.complexFixTab.Text = "Complex Fix";
+            this.complexFixTab.UseVisualStyleBackColor = true;
+            // 
+            // complexFixButton
+            // 
+            this.complexFixButton.Enabled = false;
+            this.complexFixButton.Location = new System.Drawing.Point(91, 169);
+            this.complexFixButton.Name = "complexFixButton";
+            this.complexFixButton.Size = new System.Drawing.Size(127, 23);
+            this.complexFixButton.TabIndex = 8;
+            this.complexFixButton.Text = "Fix";
+            this.complexFixButton.UseVisualStyleBackColor = true;
+            this.complexFixButton.Click += new System.EventHandler(this.complexFixButton_Click);
+            // 
+            // expected2Textbox
+            // 
+            this.expected2Textbox.Location = new System.Drawing.Point(236, 120);
+            this.expected2Textbox.Name = "expected2Textbox";
+            this.expected2Textbox.Size = new System.Drawing.Size(127, 20);
+            this.expected2Textbox.TabIndex = 7;
+            this.expected2Textbox.PlaceholderText = "Expected";
+            this.expected2Textbox.TextChanged += new System.EventHandler(this.complexTextbox_TextChanged);
+            // 
+            // inFile2Textbox
+            // 
+            this.inFile2Textbox.Location = new System.Drawing.Point(91, 120);
+            this.inFile2Textbox.Name = "inFile2Textbox";
+            this.inFile2Textbox.Size = new System.Drawing.Size(127, 20);
+            this.inFile2Textbox.TabIndex = 6;
+            this.inFile2Textbox.PlaceholderText = "In file";
+            this.inFile2Textbox.TextChanged += new System.EventHandler(this.complexTextbox_TextChanged);
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(21, 120);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(48, 13);
+            this.label5.TabIndex = 5;
+            this.label5.Text = "Subtitle2";
+            // 
+            // expected1Textbox
+            // 
+            this.expected1Textbox.Location = new System.Drawing.Point(236, 69);
+            this.expected1Textbox.Name = "expected1Textbox";
+            this.expected1Textbox.Size = new System.Drawing.Size(127, 20);
+            this.expected1Textbox.TabIndex = 4;
+            this.expected1Textbox.PlaceholderText = "Expected";
+            this.expected1Textbox.TextChanged += new System.EventHandler(this.complexTextbox_TextChanged);
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(21, 72);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(48, 13);
+            this.label4.TabIndex = 3;
+            this.label4.Text = "Subtitle1";
+            // 
+            // inFile1Textbox
+            // 
+            this.inFile1Textbox.Location = new System.Drawing.Point(91, 69);
+            this.inFile1Textbox.Name = "inFile1Textbox";
+            this.inFile1Textbox.Size = new System.Drawing.Size(127, 20);
+            this.inFile1Textbox.TabIndex = 2;
+            this.inFile1Textbox.PlaceholderText = "In file";
+            this.inFile1Textbox.TextChanged += new System.EventHandler(this.complexTextbox_TextChanged);
+            // 
+            // complexFolderPathTextBox
+            // 
+            this.complexFolderPathTextBox.Location = new System.Drawing.Point(91, 21);
+            this.complexFolderPathTextBox.Name = "complexFolderPathTextBox";
+            this.complexFolderPathTextBox.Size = new System.Drawing.Size(361, 20);
+            this.complexFolderPathTextBox.TabIndex = 1;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(21, 24);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(23, 13);
+            this.label3.TabIndex = 0;
+            this.label3.Text = "File";
             // 
             // SubtitleFixer
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Controls.Add(this.label2);
-            this.Controls.Add(this.label1);
-            this.Controls.Add(this.msTextbox);
-            this.Controls.Add(this.fixButton);
-            this.Controls.Add(this.folderPathTextBox);
+            this.Controls.Add(this.tabControl1);
             this.Name = "SubtitleFixer";
             this.Text = "Subtitle Fixer";
+            this.tabControl1.ResumeLayout(false);
+            this.simpleFixTab.ResumeLayout(false);
+            this.simpleFixTab.PerformLayout();
+            this.complexFixTab.ResumeLayout(false);
+            this.complexFixTab.PerformLayout();
             this.ResumeLayout(false);
-            this.PerformLayout();
 
         }
 
         #endregion
 
-        private System.Windows.Forms.Button fixButton;
-        private System.Windows.Forms.TextBox folderPathTextBox;
+        private System.Windows.Forms.Button simpleFixButton;
+        private System.Windows.Forms.TextBox simpleFolderPathTextBox;
         private System.Windows.Forms.TextBox msTextbox;
         private System.Windows.Forms.Label label1;
         private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TabControl tabControl1;
+        private System.Windows.Forms.TabPage simpleFixTab;
+        private System.Windows.Forms.TabPage complexFixTab;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox complexFolderPathTextBox;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.TextBox inFile1Textbox;
+        private System.Windows.Forms.TextBox expected1Textbox;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.TextBox expected2Textbox;
+        private System.Windows.Forms.TextBox inFile2Textbox;
+        private System.Windows.Forms.Button complexFixButton;
     }
 }
-
--- CasualConsole/SubtitleFixer/SubtitleFixer.cs
+++ CasualConsole/SubtitleFixer/SubtitleFixer.cs
@@ -1,22 +1,23 @@
-﻿using System;
-using System.IO;
+using System.Reflection.Metadata.Ecma335;
 using System.Text;
-using System.Windows.Forms;
 
 namespace SubtitleFixer
 {
     public partial class SubtitleFixer : Form
     {
-        private string[] fileNames;
+        private string[] simpleFileNames = new string[] { };
+        private string[] complexFileNames = new string[] { };
 
         public SubtitleFixer()
         {
             InitializeComponent();
 
-            this.folderPathTextBox.Click += new System.EventHandler(this.folderPathTextBox_Click);
+            this.simpleFolderPathTextBox.Click += new System.EventHandler(this.simpleFolderPathTextBox_Click);
+
+            this.complexFolderPathTextBox.Click += new System.EventHandler(this.complexFolderPathTextBox_Click);
         }
 
-        private void folderPathTextBox_Click(object sender, EventArgs e)
+        private void simpleFolderPathTextBox_Click(object? sender, EventArgs e)
         {
             OpenFileDialog openFileDialog = new OpenFileDialog();
 
@@ -27,31 +28,66 @@ namespace SubtitleFixer
 
             if (result == DialogResult.OK)
             {
-                this.fileNames = openFileDialog.FileNames;
+                this.simpleFileNames = openFileDialog.FileNames;
 
-                folderPathTextBox.Text = string.Join(",", this.fileNames);
+                simpleFolderPathTextBox.Text = string.Join(",", this.simpleFileNames);
 
-                Check();
+                CheckSimpleFixButton();
             }
         }
 
-        private void Check()
+        private void complexFolderPathTextBox_Click(object? sender, EventArgs e)
         {
-            var b = this.fileNames?.Length > 0 && int.TryParse(msTextbox.Text, out var millis);
+            OpenFileDialog openFileDialog = new OpenFileDialog();
 
-            fixButton.Enabled = b;
+            openFileDialog.Multiselect = false;
+            openFileDialog.Filter = "Custom Files (*.srt)|*.srt|All files (*.*)|*.*";
+
+            var result = openFileDialog.ShowDialog();
+
+            if (result == DialogResult.OK)
+            {
+                this.complexFileNames = openFileDialog.FileNames;
+
+                complexFolderPathTextBox.Text = string.Join(",", this.complexFileNames);
+
+                CheckComplexFixButton();
+            }
+        }
+
+        private void CheckSimpleFixButton()
+        {
+            var success = this.simpleFileNames?.Length > 0 && int.TryParse(msTextbox.Text, out var millis);
+
+            simpleFixButton.Enabled = success;
+        }
+
+        private void CheckComplexFixButton()
+        {
+            var success = this.complexFileNames?.Length > 0
+                && TryParseTimeSpan(expected1Textbox.Text, out var expected1Value)
+                && TryParseTimeSpan(expected2Textbox.Text, out var expected2Value)
+                && TryParseTimeSpan(inFile1Textbox.Text, out var inFile1Value)
+                && TryParseTimeSpan(inFile2Textbox.Text, out var inFile2Value);
+
+            complexFixButton.Enabled = success;
         }
 
         private void msTextbox_TextChanged(object sender, EventArgs e)
         {
-            Check();
+            CheckSimpleFixButton();
         }
 
-        private void fixButton_Click(object sender, EventArgs e)
+        private void complexTextbox_TextChanged(object sender, EventArgs e)
+        {
+            CheckComplexFixButton();
+        }
+
+        private void simpleFixButton_Click(object sender, EventArgs e)
         {
             int.TryParse(msTextbox.Text, out var millis);
 
-            var fileName = this.fileNames[0];
+            var fileName = this.simpleFileNames[0];
             var i = fileName.LastIndexOf('.');
             var writeFileName = fileName.Substring(0, i) + "_2" + fileName.Substring(i);
 
@@ -64,8 +100,8 @@ namespace SubtitleFixer
                     if (line2.Contains(divider))
                     {
                         var parts = line2.Split(new[] { divider }, StringSplitOptions.None);
-                        var t1 = Convert(parts[0]);
-                        var t2 = Convert(parts[1]);
+                        TryParseTimeSpan(parts[0], out var t1);
+                        TryParseTimeSpan(parts[1], out var t2);
 
                         t1 += TimeSpan.FromMilliseconds(millis);
                         t2 += TimeSpan.FromMilliseconds(millis);
@@ -82,13 +118,80 @@ namespace SubtitleFixer
             MessageBox.Show("Done!");
         }
 
-        private static TimeSpan Convert(string s)
+        private void complexFixButton_Click(object sender, EventArgs e)
         {
+            TryParseTimeSpan(expected1Textbox.Text, out var expectedTime1);
+            TryParseTimeSpan(expected2Textbox.Text, out var expectedTime2);
+            TryParseTimeSpan(inFile1Textbox.Text, out var inFile1);
+            TryParseTimeSpan(inFile2Textbox.Text, out var inFile2);
+
+            var off1 = inFile1 - expectedTime1;
+            var off2 = inFile2 - expectedTime2;
+            var inFileDiff = inFile2 - inFile1;
+            var offDiff = off2 - off1;
+            var ratio = offDiff / inFileDiff;
+
+            TimeSpan fixTimeSpan(TimeSpan timex)
+            {
+                var offRealx = ratio * (timex - inFile1) + off1;
+                return timex - offRealx;
+            }
+
+            var fileName = this.complexFileNames[0];
+            var i = fileName.LastIndexOf('.');
+            var writeFileName = fileName.Substring(0, i) + "_2" + fileName.Substring(i);
+
+            using (var fWrite = File.OpenWrite(writeFileName))
+            {
+                foreach (var line in File.ReadAllLines(fileName))
+                {
+                    var line2 = line;
+                    var divider = " --> ";
+                    if (line2.Contains(divider))
+                    {
+                        var parts = line2.Split(new[] { divider }, StringSplitOptions.None);
+                        TryParseTimeSpan(parts[0], out var t1);
+                        TryParseTimeSpan(parts[1], out var t2);
+
+                        t1 = fixTimeSpan(t1);
+                        t2 = fixTimeSpan(t2);
+
+                        var format = "hh\\:mm\\:ss\\,fff";
+                        line2 = t1.ToString(format) + divider + t2.ToString(format);
+                    }
+
+                    var bytes = Encoding.UTF8.GetBytes(line2 + "\n");
+                    fWrite.Write(bytes, 0, bytes.Length);
+                }
+            }
+
+            MessageBox.Show("Done!");
+        }
+
+        private static bool TryParseTimeSpan(string s, out TimeSpan timeSpan)
+        {
+            if (s.Length == 0)
+            {
+                timeSpan = default;
+                return false;
+            }
+
             var parts = s.Split(',');
+            if (parts.Length < 2)
+            {
+                timeSpan = default;
+                return false;
+            }
 
-            var t = TimeSpan.Parse(parts[0]) + TimeSpan.FromMilliseconds(int.Parse(parts[1]));
+            if (!int.TryParse(parts[1], out var millis))
+            {
+                timeSpan = default;
+                return false;
+            }
+            
+            timeSpan = TimeSpan.Parse(parts[0]) + TimeSpan.FromMilliseconds(millis);
 
-            return t;
+            return true;
         }
     }
 }
