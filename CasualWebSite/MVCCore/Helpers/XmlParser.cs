f1a4d43e6b49de85c2e65b1c50b50febcbc5a317
Serhat Abdulbakioglu <serhat359@gmail.com>
2023-07-15 14:47:26 +0900

    updated xml parser

--- CasualWebSite/MVCCore/Helpers/XmlParser.cs
+++ CasualWebSite/MVCCore/Helpers/XmlParser.cs
@@ -9,24 +9,38 @@ namespace MVCCore.Helpers
     {
         private static readonly IReadOnlySet<string> unclosedTags = new HashSet<string> { "area", "base", "br", "col", "embed", "hr", "img", "input", "link", "meta", "source", "track", "wbr", };
 
-        public static XmlRoot Parse(string xml, bool isHtml = false)
+        public static XmlNodeBase ParseHtml(string xml)
         {
-            ArraySegment<(string, int)> parts = GetParts(xml).ToArray();
+            return Parse(xml, isHtml: true);
+        }
 
-            var topDocument = new XmlRoot();
-
-            int index = 0;
-            while (true)
+        public static XmlNodeBase Parse(string xml, bool isHtml = false)
+        {
+            var partsEnumerable = GetParts(xml).Where(x => !string.IsNullOrWhiteSpace(x.token));
+            if (isHtml)
             {
-                var (node, endIndex) = ReadNode(parts[index..], isHtml);
-                topDocument.ChildNodes.Add(node);
+                if (partsEnumerable.FirstOrDefault().token.StartsWith("<!"))
+                    partsEnumerable = partsEnumerable.Skip(1);
+            }
+            ArraySegment<(string, int)> parts = partsEnumerable.ToArray();
 
-                if (endIndex < 0)
-                    throw new Exception();
-                if (endIndex + index == parts.Count)
-                    break;
+            var topDocument = new XmlNodeBase();
 
-                index += endIndex;
+            if (parts.Count > 0)
+            {
+                int index = 0;
+                while (true)
+                {
+                    var (node, endIndex) = ReadNode(parts[index..], isHtml);
+                    topDocument.AddXmlNode(node);
+
+                    if (endIndex < 0)
+                        throw new Exception();
+                    if (endIndex + index == parts.Count)
+                        break;
+
+                    index += endIndex;
+                }
             }
 
             return topDocument;
@@ -52,24 +66,25 @@ namespace MVCCore.Helpers
                 return (parent, index);
             }
 
-            while (tokens[index].token.IsBeginTag())
+            while (true)
             {
-                var (node, endIndex) = ReadNode(tokens[index..], isHtml);
-                index += endIndex;
-                parent.ChildNodes.Add(node);
+                if (tokens[index].token.IsBeginTag())
+                {
+                    var (node, endIndex) = ReadNode(tokens[index..], isHtml);
+                    index += endIndex;
+                    parent.AddXmlNode(node);
+                    continue;
+                }
+                if (!tokens[index].token.IsTag())
+                {
+                    var text = tokens[index];
+                    parent.AddTextNode(NormalizeXml(text.token));
+                    index++;
+                    continue;
+                }
+                break;
             }
-            if (!tokens[index].token.IsTag())
-            {
-                var text = tokens[index];
-                parent.InnerText = NormalizeXml(text.token);
-                index++;
-            }
-            while (tokens[index].token.IsBeginTag())
-            {
-                var (node, endIndex) = ReadNode(tokens[index..], isHtml);
-                index += endIndex;
-                parent.ChildNodes.Add(node);
-            }
+
             if (tokens[index].token != "</" + tagName + ">")
             {
                 throw new Exception($"Error on line: {tokens[index].lineNumber}. Expected '{"</" + tagName + ">"}' but encountered '{tokens[index].token}'");
@@ -86,10 +101,6 @@ namespace MVCCore.Helpers
             bool IsWhiteSpace(char c)
             {
                 if (c == '\n') lineNumber++;
-                return char.IsWhiteSpace(c);
-            }
-            static bool IsWhiteSpaceNoIncrement(char c)
-            {
                 return char.IsWhiteSpace(c);
             }
             static bool ContinuesWith(string s, string text, int index)
@@ -119,8 +130,7 @@ namespace MVCCore.Helpers
 
             while (true)
             {
-                while (i < xml.Length && IsWhiteSpace(xml[i]))
-                    i++;
+                if (i < xml.Length && xml[i] == '\n') lineNumber++;
                 if (i == xml.Length)
                     break;
 
@@ -172,7 +182,7 @@ namespace MVCCore.Helpers
                 {
                     int lineNumberStart = lineNumber;
                     int start = i;
-                    while (true)
+                    while (i < xml.Length)
                     {
                         if (xml[i] == '\n') lineNumber++;
                         if (xml[i] == '<')
@@ -181,8 +191,6 @@ namespace MVCCore.Helpers
                     }
 
                     var end = i;
-                    while (IsWhiteSpaceNoIncrement(xml[end - 1]))
-                        end--;
 
                     var token = xml[start..end];
                     yield return (token, lineNumberStart);
@@ -230,7 +238,9 @@ namespace MVCCore.Helpers
             }
         }
 
-        private static readonly Regex htmlEncodedRegex = new Regex(@"&[0-9a-zA-Z]{3,7};", RegexOptions.Compiled);
+        private static readonly Regex htmlEncodedRegex = new Regex(@"&[0-9a-zA-Z]+;", RegexOptions.Compiled);
+        private static readonly Regex htmlEncodedRegexInt = new Regex(@"&#([0-9]+);", RegexOptions.Compiled);
+        private static readonly Regex htmlEncodedRegexHexInt = new Regex(@"&#x([0-9]+);", RegexOptions.Compiled);
         private static string NormalizeXml(string s)
         {
             if (s.Length >= 2 && s[0] == '<' && s[1] == '!')
@@ -238,17 +248,54 @@ namespace MVCCore.Helpers
                 return s[9..^3];
             }
 
-            return htmlEncodedRegex.Replace(s, m =>
-            {
-                var unicode = HttpUtility.HtmlDecode(m.Value);
-                return unicode;
-            });
+            s = htmlEncodedRegex.Replace(s, m => HttpUtility.HtmlDecode(m.Value));
+            s = htmlEncodedRegexInt.Replace(s, m => ((char)int.Parse(m.Groups[1].Value)).ToString());
+            s = htmlEncodedRegexHexInt.Replace(s, m => ((char)Convert.ToUInt32(m.Groups[1].Value, 16)).ToString());
+            return s;
         }
     }
 
-    public class XmlRoot
+    public class XmlNodeBase
     {
-        public List<XmlNode> ChildNodes { get; set; } = new List<XmlNode>();
+        private readonly List<object> children = new List<object>();
+        private readonly List<XmlNode> childNodes = new List<XmlNode>();
+
+        public IReadOnlyList<object> Children => children;
+        public IReadOnlyList<XmlNode> ChildNodes => childNodes;
+        public string InnerText
+        {
+            get
+            {
+                var stringBuilder = new StringBuilder();
+                foreach (var item in Children)
+                {
+                    if (item is string s)
+                        stringBuilder.Append(s);
+                    else if (item is XmlNode node)
+                        stringBuilder.Append(node.InnerText);
+                    else
+                        throw new Exception();
+                }
+                return stringBuilder.ToString().Trim();
+            }
+            set
+            {
+                children.Clear();
+                childNodes.Clear();
+                children.Add(value);
+            }
+        }
+
+        public void AddXmlNode(XmlNode node)
+        {
+            children.Add(node);
+            childNodes.Add(node);
+        }
+
+        public void AddTextNode(string text)
+        {
+            children.Add(text);
+        }
 
         public string Beautify(string indentChars = "  ", string newLineChars = "\r\n")
         {
@@ -263,29 +310,16 @@ namespace MVCCore.Helpers
         }
     }
 
-    public class XmlNode
+    public class XmlNode : XmlNodeBase
     {
         private string? tagName;
         private NameValueCollection? attributes;
 
-        public string InnerText { get; set; } = "";
         public string TagName { get { return tagName ?? throw new Exception(); } set { tagName = value; } }
         public NameValueCollection Attributes { get { return attributes ?? throw new Exception(); } set { attributes = value; } }
-        public List<XmlNode> ChildNodes { get; set; } = new List<XmlNode>();
 
         public XmlNode()
         {
-        }
-
-        public XmlNode(string tagName, NameValueCollection attributes)
-        {
-            this.tagName = tagName;
-            this.attributes = attributes;
-        }
-
-        public void AppendChild(XmlNode node)
-        {
-            ChildNodes.Add(node);
         }
 
         public void Beautify(StringBuilder sb, string indentChars = "  ", string newLineChars = "\r\n")
@@ -314,34 +348,31 @@ namespace MVCCore.Helpers
                 }
                 sb.Append('>');
 
-                sb.Append(HttpUtility.HtmlEncode(node.InnerText));
+                bool isSingleAndString = node.Children.Count == 1 && node.ChildNodes.Count == 0;
+                foreach (var item in node.Children)
+                {
+                    if (!isSingleAndString)
+                        sb.Append(newLineChars);
+                    if (item is string s)
+                        sb.Append(item);
+                    else if (item is XmlNode n)
+                        Write(n, level + 1);
+                    else
+                        throw new Exception();
+                }
 
-                foreach (var item in node.ChildNodes)
+                if (!isSingleAndString)
                 {
                     sb.Append(newLineChars);
-                    Write(item, level + 1);
-                }
-                if (node.ChildNodes.Any())
-                    sb.Append(newLineChars);
-
-                if (node.ChildNodes.Any())
                     for (int i = 0; i < level; i++)
                         sb.Append(indentChars);
+                }
                 sb.Append("</");
                 sb.Append(node.tagName);
                 sb.Append('>');
             }
 
             Write(node, 0);
-        }
-
-        public string Beautify(string indentChars = "  ", string newLineChars = "\r\n")
-        {
-            var sb = new StringBuilder();
-
-            Beautify(sb, indentChars, newLineChars);
-
-            return sb.ToString();
         }
     }
 
@@ -365,6 +396,19 @@ namespace MVCCore.Helpers
         public static bool IsSingleTag(this string s)
         {
             return s[^1] == '>' && s[^2] == '/';
+        }
+    }
+
+    public static class XmlNodeExtensions
+    {
+        public static IEnumerable<XmlNode> AllInnerNodes(this XmlNodeBase root)
+        {
+            foreach (var item in root.ChildNodes)
+            {
+                yield return item;
+                foreach (var node in item.AllInnerNodes())
+                    yield return node;
+            }
         }
     }
 }
