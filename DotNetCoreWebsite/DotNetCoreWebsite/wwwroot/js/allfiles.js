d1247beeae8f8f623eec68eb1321fc6718bf4a43
Serhat Abdulbakioglu <serhat359@gmail.com>
2024-01-17 20:29:50 +0900

    refactor

--- DotNetCoreWebsite/DotNetCoreWebsite/wwwroot/js/allfiles.js
+++ DotNetCoreWebsite/DotNetCoreWebsite/wwwroot/js/allfiles.js
@@ -1,7 +1,7 @@
 ﻿var isSet = false;
 
 function onMainCheckboxClick() {
-    let allCheckBoxes = document.querySelectorAll(".downloadCheckBox input[type=checkbox]");
+    const allCheckBoxes = document.querySelectorAll(".downloadCheckBox input[type=checkbox]");
 
     isSet = !isSet;
     for (let i = 0; i < allCheckBoxes.length; i++) {
@@ -17,12 +17,12 @@ function onCheckboxClick() {
 }
 
 function getDownloadSelectedUrl() {
-    let path = new URL(window.location.toString()).searchParams.get("q");
-    let fileNames = [];
-    let allSelectedCheckBoxes = document.querySelectorAll(".downloadCheckBox input[type=checkbox]:checked");
+    const path = new URL(window.location.toString()).searchParams.get("q");
+    const fileNames = [];
+    const allSelectedCheckBoxes = document.querySelectorAll(".downloadCheckBox input[type=checkbox]:checked");
 
     for (let i = 0; i < allSelectedCheckBoxes.length; i++) {
-        let link = allSelectedCheckBoxes[i].parentElement.parentElement.querySelector(".downloadLink a").href;
+        const link = allSelectedCheckBoxes[i].parentElement.parentElement.querySelector(".downloadLink a").href;
         let fileName = new URL(link).searchParams.get("q");
 
         if (path) {
@@ -32,7 +32,7 @@ function getDownloadSelectedUrl() {
         fileNames.push(fileName);
     }
 
-    let params = fileNames.map(x => "q=" + encodeURIComponent(x));
+    const params = fileNames.map(x => "q=" + encodeURIComponent(x));
 
     if (path)
         params.push("path=" + encodeURIComponent(path));
@@ -41,30 +41,30 @@ function getDownloadSelectedUrl() {
 }
 
 function getDownloadLinks() {
-    let linksArray = Array.from(document.querySelectorAll(".downloadCheckBoxInput:checked")).map(x => x.closest("tr").querySelector(".downloadLink a").href);
-    let linksJoined = linksArray.join("<br />");
+    const linksArray = Array.from(document.querySelectorAll(".downloadCheckBoxInput:checked")).map(x => x.closest("tr").querySelector(".downloadLink a").href);
+    const linksJoined = linksArray.join("<br />");
     document.getElementById("linksModalContent").innerHTML = linksJoined;
     makeModal("#linksModal");
 }
 
 function setDownloadButtonVisibility() {
-    let checkedCount = document.querySelectorAll('.downloadCheckBoxInput:checked').length;
+    const checkedCount = document.querySelectorAll('.downloadCheckBoxInput:checked').length;
     setVisible('#downloadSelectedButton', checkedCount > 0);
     setVisible('#getDownloadLinksButton', checkedCount > 0);
 }
 
 function toLocalDate(utcDateString) {
-    let date = utcDateString.replace(" UTC", "").replace(" ", "T") + ":00.000Z";
+    const date = utcDateString.replace(" UTC", "").replace(" ", "T") + ":00.000Z";
     return formatDate(new Date(date));
 }
 
 function formatDate(d) {
-    let month = '' + (d.getMonth() + 1);
-    let day = '' + d.getDate();
-    let year = d.getFullYear();
+    const month = '' + (d.getMonth() + 1);
+    const day = '' + d.getDate();
+    const year = d.getFullYear();
 
-    let hours = d.getHours();
-    let minutes = d.getMinutes();
+    const hours = d.getHours();
+    const minutes = d.getMinutes();
 
     return `${year}-${pad2Left(month)}-${pad2Left(day)} ${pad2Left(hours)}:${pad2Left(minutes)}`;
 }
@@ -74,22 +74,22 @@ function pad2Left(n) {
 }
 
 function convertDatesToLocal() {
-    let allDates = document.querySelectorAll(".utcdate");
-    for (x of allDates) {
+    const allDates = document.querySelectorAll(".utcdate");
+    for (const x of allDates) {
         x.innerHTML = toLocalDate(x.innerHTML);
     }
 }
 
 async function getFolderSizes() {
-    let response = await fetch("/Home/GetFolderSize" + document.location.search);
+    const response = await fetch("/Home/GetFolderSize" + document.location.search);
     if (!response.ok)
         throw new Error();
-    let data = await response.json();
-    let rows = document.querySelectorAll("table tbody tr.xfolder");
-    for (let row of rows) {
-        let tds = row.querySelectorAll("td");
-        let folderName = tds[1].querySelector("a").innerText;
-        let sizeInfo = data[folderName];
+    const data = await response.json();
+    const rows = document.querySelectorAll("table tbody tr.xfolder");
+    for (const row of rows) {
+        const tds = row.querySelectorAll("td");
+        const folderName = tds[1].querySelector("a").innerText;
+        const sizeInfo = data[folderName];
         tds[2].innerText = sizeInfo.fileSizeString;
         tds[2].dataset["sortValue"] = sizeInfo.fileSize;
     }
@@ -115,8 +115,8 @@ document.addEventListener("DOMContentLoaded", function (event) {
 
     addOnClick('.downloadCheckBoxInput', function (event) {
         setDownloadButtonVisibility();
-    })
-
+    });
+    
     addOnClick('#selectAll', function (event) {
         selectText('linksModalContent');
     });
@@ -127,15 +127,15 @@ document.addEventListener("DOMContentLoaded", function (event) {
 });
 
 function setVisible(selector, isVisible) {
-    let elems = document.querySelectorAll(selector);
-    for (let elem of elems) {
+    const elems = document.querySelectorAll(selector);
+    for (const elem of elems) {
         elem.style.visibility = isVisible ? "visible" : "hidden";
     }
 }
 
 function addOnClick(selector, func) {
-    let elems = document.querySelectorAll(selector);
-    for (let elem of elems) {
+    const elems = document.querySelectorAll(selector);
+    for (const elem of elems) {
         elem.addEventListener("click", func, false);
     }
 }
@@ -143,10 +143,10 @@ function addOnClick(selector, func) {
 function allowTableSorting() {
     // Allow table sorting
     addOnClick('th', function () {
-        let table = this.closest("table");
-        let tbody = table.querySelector("tbody");
-        let trs = tbody.querySelectorAll('tr');
-        let comparer = getComparer(whichIndex(this));
+        const table = this.closest("table");
+        const tbody = table.querySelector("tbody");
+        const trs = tbody.querySelectorAll('tr');
+        const comparer = getComparer(whichIndex(this));
         let rows = Array.from(trs).sort(comparer);
         this.asc = !this.asc;
         if (!this.asc) { rows = rows.reverse() }
@@ -158,14 +158,14 @@ function allowTableSorting() {
 
 function getComparer(index) {
     return function (a, b) {
-        let valA = getCellValue(a, index), valB = getCellValue(b, index)
+        const valA = getCellValue(a, index), valB = getCellValue(b, index)
         return isNumeric(valA) && isNumeric(valB) ? valA - valB : valA.toString().localeCompare(valB)
     }
 }
 
 function getCellValue(row, index) {
-    let elem = row.querySelectorAll("td")[index];
-    let dataValue = elem.dataset["sortValue"];
+    const elem = row.querySelectorAll("td")[index];
+    const dataValue = elem.dataset["sortValue"];
     if (dataValue) return dataValue;
     return elem.innerText;
 }
@@ -175,7 +175,7 @@ function isNumeric(text) {
 }
 
 function whichIndex(elem) {
-    let children = elem.parentElement.children;
+    const children = elem.parentElement.children;
     for (let i = 0; i < children.length; i++)
         if (children[i] == elem)
             return i;
@@ -183,8 +183,8 @@ function whichIndex(elem) {
 }
 
 function makeModal(selector) {
-    var modal = document.querySelector(selector);
-    var container = modal.querySelector(".modal-container");
+    const modal = document.querySelector(selector);
+    const container = modal.querySelector(".modal-container");
 
     modal.classList.remove("modal-hidden");
 
